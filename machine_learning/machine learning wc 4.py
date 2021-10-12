6258a21a806572d88c21b46ed1aadaef1a02bd03
Sam <ssuidman@gmail.com>
2021-10-12 23:37:01 +0200

    machine learning wc 4 verder

--- machine_learning/machine learning wc 4.py
+++ machine_learning/machine learning wc 4.py
@@ -31,22 +31,41 @@ t7 = np.ones([1,n7])[0] #length = 6265
 
 
 #these are the functions that are defined in the exercise 
-def y(w,X):
+
+def y(w,X): #this function takes a random w and a matrix with N patterns and d dimensions (in our case 785 pixels in total) and returns an array that contains for each pattern the probability that the number is a 3 (or 7)
     y = np.array([1/(1+np.exp(-np.dot(w,x))) for x in X])
     return y 
 
-def E(w,X,t):
-    Ew = np.array([-1/len(x)*(t*np.log(y(w,x)+(1-t)*np.log(1-y(w,x)))) for x in X])
+def E(w,X,t): #Return the error for w,X and the actual value 3,7 (so actual 1,0)
+    y0 = y(w,X)
+    Ew = -np.sum(t*np.log(y0)+(1-t)*np.log(1-y0))/len(X)
     return Ew
 
-def dE(w,X,t):
-    dE = np.array([1/len(x)*(y(w,x)-t)*x for x in X])
+def dE(w,X,t): #gradient for w,X,t
+    y0 = y(w,X)
+    dE = np.matmul((y0-t),X)/len(X)
     return dE
 
+def H(w,X,t): #Hessian for w,X,t
+    y0 = y(w,X) 
+    X_y = np.multiply(X.transpose(),t).transpose() #multiply each pattern in X by the y value of that pattern. This results in a matrix. 
+    H = np.matmul(X_y.transpose(),X) #multiply this X*y times X, and sum over all patterns. The new shape is then (785,785)
+    return H
 
 
-w = np.random.normal(0,1,size=X3[0].shape)
-t = np.random.randint(2,size=X3[0].shape)
+w = np.random.normal(0,1,size=X3.shape[1])
+t = np.random.randint(2,size=X3.shape[0])
 
+
+#iterating
+
+import time
+
+t1 = time.time()
+for i in range(10000):
+    w += -0.01*dE(w,X3,t)
+    if i%100==0:
+        t2 = time.time()
+        print(i,round(t2-t1),round(E(w,X3,t),2))
 
 
