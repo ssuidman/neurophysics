cd4b592f80cb5a123bead59bba139577924b5adf
Sam Suidman <sam@ip-145-137-147-103.wlan-int.ru.nl>
2023-09-22 16:51:35 +0200

    Wim's quickscore algoritme best ver in julia al, alleen pfplus klopt nog niet

---
+++ stage_promedas/quickscore_algorithm.jl
@@ -0,0 +1,39 @@
+# function fun_quickscoreW(previn, pfmin, pfminneg)
+# Wim Wiegerinck version 8/9/2023
+# inputs:
+# previn               prevalences
+# pfmin = 1 - sens     (for positive tests): these are Heckerman's p(f-|d)
+# pfminneg             (products of 1-sens for negatives, to be absorbed by prevs): these are hekkermans Prod_{F-} (p(f-|d))
+# outputs:
+# posterior =  [P(d_1=1|F+,F-),... P(d_n=1|F+,F-)]
+# pfpluspd = [p(F+,F-), P(F+,F-|d_1=1)p(d_1=1),..., P(F+,F-|d_n=1)p(d_n=1)]
+# pfplus = [ p(F+,F-), P(F+,F-|d_1=1),..., P(F+,F-|d_n=1) ]
+
+m, n = !isempty(pfmin) ? size(pfmin) : (0, length(previn)) # m number of postests. n number of diags. if size(postest) is 0, then we set m=0 `` 
+pfplus = zeros(n+1)  # will be used for P(F+,F-), P(F+,F-|d_1=1),... P(F+,F-|d_n=1) (note F- will be absorbed below) 
+prev = repeat(previn',inner=(41,1)) # copies of prevalences
+for i in 1:n prev[i+1,i]=1 end # set the (i+1,i) entry 1 to condition on d_i=1,  etc. 
+prevend = vcat(1,previn) # prevend are the prevalences 1, P(d_1=1),... P(d_n=1) and is needed to multiply at the end
+prevminneg = !isempty(pfminneg) ? prev.*pfminneg : prev.*ones(n+1,n) # absorb negative findings in prevminneg (so these are p(F-|d_1=1)p(d_1=1), ... p(F-|d_n=1)p(d_n=1) ), which is needed in heckerman eqn 11 (compare with heckerman 10, there is a constant difference which is incorporated in the block below)
+
+myset = 0
+for i in 1:(2^m-1) # iterate over 2^m possibilities 
+    v = digits(i,base=2,pad=8) # create vector of 0's and 1's from binary number i 
+    myset = findall(v.==1) # find places of the 1-elements
+    if length(myset)==1
+        pfplus .+= ((-1) ^ length(myset)) .* exp.(sum(log.(1e-50 .+ pfmin[myset, :] .* prevminneg .+ (1 .- prev)), dims=2))
+    else
+        pfplus .+= ((-1) ^ length(myset)) .* exp.(sum(log.(1e-50 .+ (prod(pfmin[myset, :]) .* prevminneg .+ (1 .- prev))), dims=2))
+    end
+end
+
+# final results
+# pfplus is p(F+,F-), P(F+,F-|d_1=1),... P(F+,F-|d_n=1),
+pfpluspd = pfplus .* prevend  # numerator in heckerman (12)  p(F+,F-),P(F+,F-|d_1=1)p(d_1=1),..., P(F+,F-|d_n=1)p(d_n=1)
+# the 2nd until (n+1)nd element are the terms for d_1=1,...,d_n=1
+# and for all the elements, normalization is done with p(F+,F-).
+posterior = (pfpluspd[2:(n+1)] ./ pfpluspd[1])'
+
+# which is the most important output: posterior = P(d_1=1|F+,F-),... P(d_n=1|F+,F-)
+# return posterior, pfpluspd, pfplus
+# end
--- stage_promedas/quickscore_preparation.jl
+++ stage_promedas/quickscore_preparation.jl
@@ -1,60 +1,50 @@
-using Random
+# To use the function, run the following lines: 
+# using Random
+# include("quickscore_preparation.jl")
+# previn,pfmin,pfminneg = quickscore_preparation()
 
-# parameters
-n_disease = 40   # number of diseases
-n_test = 30      # number of findings
-expected_number_of_diseases = 3  # typical number of diags
-sparseness1 = 0.5  # connection density
+function quickscore_preparation(n_disease=40,n_test=30,expected_number_of_diseases=3,sparseness1=0.5)
+    # the input parameters are 'typical number of diags', 'number of findings', 'typical number of diags', 'connection density'
 
-# the model
-prev = (rand(1, n_disease) .+ 1) / 2  # prevalences of diseases, random between 0.5 and 1
-prev .= expected_number_of_diseases * prev / sum(prev)  # in expectation sumofdiags diseases present
+    # the model
+    prev = (rand(n_disease) .+ 1) / 2  # prevalences of diseases, random between 0.5 and 1
+    prev .= expected_number_of_diseases * prev / sum(prev)  # in expectation sumofdiags diseases present
 
+    connections = rand(n_test, n_disease) .< sparseness1  # selection of connections
+    sensorig = (1 .+ 4 .* rand(n_test, n_disease)) ./ 10 .* connections  # sensitivities random between 0.1 and 0.5
 
-connections = rand(n_test, n_disease) .< sparseness1  # selection of connections
-sensorig = (1 .+ 4 .* rand(n_test, n_disease)) ./ 10 .* connections  # sensitivities random between 0.1 and 0.5
+    # data generation. Generate diseases according to the prevalences. Then test results (=findings) according to the sensitivities.
+    # accept if the number of diseases and the number of positive findings is OK, else try again.
+    actualdiags, postest, negtest, ndiags,m,testresults = 0,0,0,0,0,0 #setting some variables to use outside the loop too 
+    while (ndiags==0) | !(m>5 && m<15) # changing the m range, could change the amount postest/negtest which both should never be empty 
+        actualdiags = findall(rand(n_disease) .< prev)  # generate diseases and get the y value out of a 1D-array of cartesian coordinates
+        ndiags = length(actualdiags) 
 
-#=
-# data generation. Generate diseases according to the prevalences. Then
-# test results (=findings) according to the sensitivities.
-# accept if the number of diseases and the number of positive findings is OK,
-# else try again.
-flag = true
-while flag
-    actualdiags = find(rand(1, n_disease) .< prev)  # generate diseases
-    ndiags = length(actualdiags)
+        testresults = vec( any(rand(n_test, ndiags).<(sensorig[:,actualdiags]), dims=2) )  # generate test results
 
-    testresults = any(rand(n_test, ndiags) .< (sensorig[:, actualdiags]), dims=2)  # generate test results
-
-    postest = findall(testresults .== 1)
-    negtest = findall(testresults .== 0)
-    m = length(postest)
-    println([ndiags, m])
-    if (m > 5) && (m < 15) && (!isempty(actualdiags))
-        # for now, only cases with at least 5 positive tests, but no more than
-        # 14 positive tests, and at least one disease.
-        flag = false
+        postest = findall(testresults.==1)
+        negtest = findall(testresults.==0)
+        m = length(postest)
     end
-end
 
-# gather data (some data preparation for the Quickscore algorithm)
-sens = sensorig[postest, :]
-sensneg = sensorig[negtest, :]
+    # gather data (some data preparation for the Quickscore algorithm)
+    sens = sensorig[postest, :]
+    sensneg = sensorig[negtest, :]
 
-pfmin = 1 .- sens
-pfminneg = prod(1 .- sensneg, dims=1)  # be careful with the dimensions: scalar result prod(1 - sensneg) for singleton neg test is wrong.
+    pfmin = 1 .- sens 
+    pfminneg = prod(1 .- sensneg, dims=1)  # be careful with the dimensions: scalar result prod(1 - sensneg) for singleton neg test is wrong.
 
-# Here it goes into quickscore.
-tic()
-(posterior, pfpluspd, pfplus) = fun_quickscoreW(prev, pfmin, pfminneg)
-timep = toq()
+    return prev, pfmin, pfminneg
+end 
 
-actualdiseases = zeros(size(prev))
-actualdiseases[actualdiags] .= 1
-println("prevalences, posteriors, actual diseases")
-println([prev', posterior', actualdiseases'])
-println("\nPositive findings: $m, time: $timep")
-```
+# # Here it goes into quickscore.
+# tic()
+# (posterior, pfpluspd, pfplus) = fun_quickscoreW(prev, pfmin, pfminneg)
+# timep = toq()
 
-Please note that there may be some differences in indexing and broadcasting between MATLAB and Julia, so I've adjusted the code accordingly. Also, Julia uses 1-based indexing, so some array indices have been adjusted accordingly.
-=#
+# actualdiseases = zeros(size(prev))
+# actualdiseases[actualdiags] .= 1
+# println("prevalences, posteriors, actual diseases")
+# println([prev', posterior', actualdiseases'])
+# println("\nPositive findings: $m, time: $timep")
+# ```
