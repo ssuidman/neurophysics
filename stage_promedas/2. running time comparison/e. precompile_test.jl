4e606fd7079158f590824ccd6368a51b1b52cba7
Sam Suidman <sam@MacBook-Air-van-Sam.local>
2024-02-20 14:55:22 +0100

    veel opgeschoten en nu bijna klaar met resultaten in overleaf al

--- stage_promedas/2. running time comparison/e. precompile_test.jl
+++
@@ -1,7 +0,0 @@
-include("../0. useful/packages.jl")
-include("../0. useful/quickscore_algorithm.jl")
-include("../0. useful/run_one_time.jl")
-
-# This file is used such that "c. helpful_tools.jl" can use it to look at precompiling 
-previn, pfmin, pfminneg, pfplus_, P_joint_, posterior_, dt_, prev, prevminneg, myset = run_one_time_var(m=17,n_myset=3);
-pfplus, P_joint, posterior, dt = quickscore(previn,pfmin,pfminneg,"exp-sum-log");
--- stage_promedas/7. Fortran implementatie/a. hello_world.f90
+++
@@ -1,8 +0,0 @@
-! gfortran a.\ hello_world.f90 -o a.\ hello.o
-! ./a.\ hello
-
-program hello
-    ! This is how you write a comment line
-    print *, "Hello, World!"
-end program hello
-  
